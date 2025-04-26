dfbd2a3886c85dbd8993f3a63adb351fe4f7f4bf
nihish-2005 <nithnithish500@gmail.com>
2025-04-26 09:18:05 -0400

    data generator added

---
+++ data_generator.py
@@ -0,0 +1,15 @@
+# data_generator.py
+import random
+import time
+
+def generate_sensor_data():
+    return {
+        'vibration': random.uniform(0, 10),
+        'strain': random.uniform(0, 500),
+        'temperature': random.uniform(20, 80)
+    }
+
+if __name__ == "__main__":
+    while True:
+        print(generate_sensor_data())
+        time.sleep(1)
